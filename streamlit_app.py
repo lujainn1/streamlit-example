7eba88f6d5002c5d18574f091a59136ea6497701
lujainn1 <92752276+lujainn1@users.noreply.github.com>
2021-10-21 12:30:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,13 @@ from PIL import Image
 
 st.title('Fntech')
 
-def load_img(uploaded_img):
-   image = Image.open(uploaded_img)
-   return img
 
 uploaded_img = st.file_uploader("Choose a file", type=["png","jpg","jpeg"])
 if uploaded_img is not None:
-   st.image(load_img, caption='AIOutput')
-
+ 
 
 #Input (uploaded_img , 2- uploaded_imgg)
 #AI 
 #Output (image)
+image = Image.open(uploaded_img)
+st.image(img, caption='AIOutput')
