67475cdcbedd6591f554a748fe8bf051ba276eaf
lujainn1 <92752276+lujainn1@users.noreply.github.com>
2021-10-21 10:45:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,8 @@ import pandas as pd
 import streamlit as st
 
 """
-# Welcome to Streamlit!
+Fntech
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -30,3 +24,20 @@ if uploaded_file is not None:
    # Can be used wherever a "file-like" object is accepted:
    dataframe = pd.read_csv(uploaded_file)
    st.write(dataframe)
+   uploaded_file = st.file_uploader("Choose a file")
+   
+if uploaded_file is not None:
+   # To read file as bytes:
+   bytes_data = uploaded_file.getvalue()  
+   st.write(bytes_data)
+   # To convert to a string based IO:
+   stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+   st.write(stringio)
+
+   # To read file as string:
+   string_data = stringio.read()
+   st.write(string_data)
+   # Can be used wherever a "file-like" object is accepted:
+   dataframe = pd.read_csv(uploaded_file)
+   st.write(dataframe)
+
