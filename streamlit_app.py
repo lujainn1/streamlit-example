63fa265835c413fc8279f25655652dfaca3ab1fc
lujainn1 <92752276+lujainn1@users.noreply.github.com>
2021-10-21 12:00:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.title('Fntech')
 
 uploaded_img = st.file_uploader("Choose a file", type=["png","jpg","jpeg"])
 
-uploaded_img = st.file_uploader2("Choose a file", type=["png","jpg","jpeg"])
-if uploaded_img is not None:
+uploaded_imgg = st.file_uploaderr("Choose a file", type=["png","jpg","jpeg"])
+if uploaded_imgg is not None:
 
 
 
