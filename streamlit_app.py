d321bd0d66e3c224c72d97e7d16d8cf92c6701e3
lujainn1 <92752276+lujainn1@users.noreply.github.com>
2021-10-21 12:29:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,9 @@ def load_img(uploaded_img):
 
 uploaded_img = st.file_uploader("Choose a file", type=["png","jpg","jpeg"])
 if uploaded_img is not None:
-
+   st.image(load_img, caption='AIOutput')
 
 
 #Input (uploaded_img , 2- uploaded_imgg)
 #AI 
 #Output (image)
-
-
-   st.image(load_img, caption='AIOutput')
