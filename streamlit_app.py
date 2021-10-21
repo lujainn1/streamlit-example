5f5ffd4fc737e503aa12066d4735e5b67901f361
lujainn1 <92752276+lujainn1@users.noreply.github.com>
2021-10-21 09:18:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,23 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+>>> uploaded_file = st.file_uploader("Choose a file")
+>>> if uploaded_file is not None:
+...     # To read file as bytes:
+...     bytes_data = uploaded_file.getvalue()
+...     st.write(bytes_data)
+>>>
+...     # To convert to a string based IO:
+...     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+...     st.write(stringio)
+>>>
+...     # To read file as string:
+...     string_data = stringio.read()
+...     st.write(string_data)
+>>>
+...     # Can be used wherever a "file-like" object is accepted:
+...     dataframe = pd.read_csv(uploaded_file)
+...     st.write(dataframe)
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
